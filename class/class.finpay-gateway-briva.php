1bc1f1f4837b542d35414cdfa06943cbc6ba66c3
unknown <finpayindo@gmail.com>
2019-11-08 11:11:02 +0700

    add local timezone

--- class/class.finpay-gateway-briva.php
+++ class/class.finpay-gateway-briva.php
@@ -3,6 +3,9 @@
 * briva / Felisa BRI Closed
 */
 
+date_default_timezone_set('Asia/Jakarta');
+// echo 'Indonesian Timezone: ' . date('d-m-Y H:i:s');
+
 class WC_Gateway_Finpay_Briva extends WC_Payment_Gateway {
 
   /**
